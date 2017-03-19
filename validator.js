34d292febce901a47389069b3bccd38879150fa2
Szymon Maruda <szymon.maruda@gmail.com>
2017-03-19 21:50:05 +0100

    form and validation done

--- validator.js
+++ validator.js
@@ -72,8 +72,8 @@ function validateRok(){
 function validateCena(){
   var x = document.getElementById('Cena').value;
   var bladNazwa = document.getElementById('cena_alert');
-  var decimal_pattern = /\d*\.\d{2}/;
-  var words_patt = /[0-9]*/;
+  var digits_patt = /[0-9]*/;
+  var words_patt = /[a-z][A-Z]*/;
 
 
   if(x==""){
@@ -84,12 +84,14 @@ function validateCena(){
         bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może zawierać liter!</p>';
         bladNazwa.setAttribute("style","display:block;");
         console.log("jestem tu");
-      }else if(words_patt.test(x)){
+      }
+      else if(digits_patt.test(x)){
         var x=parseFloat(x).toFixed(2);
         document.getElementById('Cena').value=x;
         bladNazwa.setAttribute("style", "display:none");
+        console.log("decimal");
       }
-      else if(decimal_pattern.test(x)){
+      else{
         console.log("jestem tu2");
         bladNazwa.setAttribute("style","display:none");
       }
@@ -107,4 +109,71 @@ function validatePaliwo(){
   else{
     bladNazwa.setAttribute("style","display:none");
   }
+}
+
+function validateKolor(){
+  var x = document.getElementById("Kolor").value;
+  var bladNazwa = document.getElementById('kolor_alert');
+  var pattern = /[0-9]/i;
+
+  if(x==0){
+    bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może być puste</p>';
+    bladNazwa.setAttribute("style","display:block");
+  }else{
+    if(pattern.test(x)){
+      bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Kolor nie może zawierać liczb</p>';
+      bladNazwa.setAttribute("style","display:block");
+    }
+    else if (x.length > 15) {
+      bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Kolor nie może być dłuższy niż 15 znaków</p>';
+      bladNazwa.setAttribute("style","display:block");
+    }
+    else{
+      bladNazwa.setAttribute("style","display:none");
+    }
+  }
+}
+
+function validatePojemnosc(){
+  var x = document.getElementById('Pojemnosc').value;
+  var bladNazwa = document.getElementById('pojemnosc_alert');
+  var digits_patt = /[0-9]*/;
+  var words_patt = /[a-z][A-Z]*/;
+
+  if(x==""){
+    bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może być puste</p>';
+    bladNazwa.setAttribute("style","display:block");
+  }else{
+      if(words_patt.test(x)){
+        bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może zawierać liter!</p>';
+        bladNazwa.setAttribute("style","display:block;");
+      }
+      else if(digits_patt.test(x)){
+        var x=parseFloat(x).toFixed(1);
+        document.getElementById('Pojemnosc').value=x;
+        bladNazwa.setAttribute("style", "display:none");
+      }
+      else{
+        bladNazwa.setAttribute("style","display:none");
+      }
+  }
+}
+
+function validatePrzebieg(){
+  var x = document.getElementById('Przebieg').value;
+  var bladNazwa = document.getElementById('przebieg_alert');
+  var words_patt = /[a-z][A-Z]*/;
+
+  if(x==""){
+    bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może być puste</p>';
+    bladNazwa.setAttribute("style","display:block");
+  }else{
+      if(words_patt.test(x)){
+        bladNazwa.innerHTML='<p class="alert alert-danger" role="alert">Pole nie może zawierać liter!</p>';
+        bladNazwa.setAttribute("style","display:block;");
+      }
+      else{
+        bladNazwa.setAttribute("style","display:none");
+      }
+    }
 }
